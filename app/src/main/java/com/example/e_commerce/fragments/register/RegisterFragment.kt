4d176b7b623e8a90b48250b48821b13e85cf5417
alisafwat <alisafwat11@gmail.com>
2025-05-06 03:15:10 +0300

    add: register screen top logo

--- app/src/main/java/com/example/e_commerce/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/register/RegisterFragment.kt
@@ -1,13 +1,21 @@
 package com.example.e_commerce.fragments.register
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.e_commerce.core.navigation.Register
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.e_commerce.R
+import com.example.e_commerce.core.AppPadding
+import com.example.e_commerce.ui.theme.EcommerceTheme
 
 @Composable
 fun RegisterFragment() {
@@ -15,15 +23,31 @@ fun RegisterFragment() {
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.primary)
+            .padding(AppPadding.medium),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Image(
+            painterResource(R.drawable.route_logo),
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(R.string.route_logo),
+        )
+        RegisterForm()
     }
 }
 
 
 @Composable
-fun RegisterForm(){
+fun RegisterForm() {
+    Column(){
 
+    }
 }
 
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegisterScreenPreview() {
+    EcommerceTheme {
+        RegisterFragment()
+    }
+}
