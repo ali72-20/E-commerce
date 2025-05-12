34758c45aa6d2a8598e0136f84546691cfadd3b4
alisafwat <alisafwat11@gmail.com>
2025-05-12 17:57:18 +0300

    feat: add signup method to AuthRemoteDataSource and its implementation

--- data/src/main/java/com/example/data/data_sources/AuthRemoteDataSource.kt
+++ data/src/main/java/com/example/data/data_sources/AuthRemoteDataSource.kt
@@ -1,8 +1,12 @@
 package com.example.data.data_sources
 
 import com.example.data.models.request_models.LoginRequestModel
+import com.example.data.models.request_models.SignupRequestModel
 import com.example.data.models.response_models.LoginResponseModel
+import com.example.data.models.response_models.SignupResponseModel
 
 interface AuthRemoteDataSource {
     suspend fun login(loginRequestModel: LoginRequestModel): LoginResponseModel
+
+    suspend fun signup(signupRequestModel: SignupRequestModel): SignupResponseModel
 }
